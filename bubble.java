346064785e0112d546ca7f79fb10436f22e89821
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-21 20:58:10 -0300

    Update bubble.java

--- bubble.java
+++ bubble.java
@@ -2,46 +2,37 @@ ublic class ArrayBurbuja {
 	
 		private long[] a; 
 		private int nElementos;  // variable elementos del array
-		//--------------------------------------------------------------
-		public ArrayBurbuja(int max)
-		 // constructor
-		{
+		
+		public ArrayBurbuja(int max){ // constructor
 		a = new long[max];
-		nElementos = 0;       // inicializacion del array
+		nElementos = 0;       // inicializacion de la variable que se desplaza x el array
 		}
-		//--------------------------------------------------------------
-		public void insert(long value)
-		 // agrega elementos dentro del  array
-		{
+		public void insert(long value){	 // metodo que agrega elementos dentro del  array
 		a[nElementos] = value;
 		 // inserta elemento
 		nElementos++;
 		 // incrementa
 		}
-		//--------------------------------------------------------------
-		public void mostrar()
+		
+		public void mostrar(){
 		 // mostrar contenido del array 
-		{
-		for(int j=0; j<nElementos; j++)   	 // for each  ,
-		System.out.print(a[j] + " ");        
-		System.out.println(" ");
+		
+		for(int j=0; j<nElementos; j++){   	 // for each  ,
+			System.out.print(a[j] + " ");        
+			System.out.println(" ");
+			}
 		}
-		//--------------------------------------------------------------
-		public void burbuja()
-		{
+		
+		public void burbuja(){
 		int salida, entrada;
-		for(salida=nElementos-1; salida>1; salida--)
-		 // bucle exterior (Hacia atras)
-		for(entrada=0; entrada<salida; entrada++)
-		 // bucle interior (Hacia Adelante)
-		if( a[entrada] > a[entrada+1] )
-		 // fuera de orden?
+		for(salida=nElementos-1; salida>1; salida--) // bucle exterior (Hacia atras)
+		   for(entrada=0; entrada<salida; entrada++) // bucle interior (Hacia Adelante)
+			if( a[entrada] > a[entrada+1] ) // fuera de orden?
 		intercambio(entrada, entrada+1);
 		 // Intercambio
 		} // Fin Metodo burbuja()
-		//--------------------------------------------------------------
-		private void intercambio(int uno, int dos) //metodo de intercambio
-		{
+	
+		private void intercambio(int uno, int dos){ //metodo de intercambio
 		long aux = a[uno];
 		a[uno] = a[dos];
 		a[dos] = aux;
