ac689dc04b0a622ee302704ff23fb9790cc656ad
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-21 23:45:14 -0300

    Update bubble.java

--- bubble.java
+++ bubble.java
@@ -1,34 +1,37 @@
 ublic class ArrayBurbuja {
 	
-		private long[] a; 
-		private int nElementos;  // variable elementos del array
+		/*
+		*Author Nordevelop
+		*/
+	        private long[] a; //objetos encapsulado con acceso denegado en la clase Main()
+		private int nElementos;  // variable entera que se dezplazara por el array
 		
-		public ArrayBurbuja(int max){ // constructor
+		public ArrayBurbuja(int max){ //clase constructor
 		a = new long[max];
 		nElementos = 0;       // inicializacion de la variable que se desplaza x el array
 		}
-		public void insert(long value){	 // metodo que agrega elementos dentro del  array
-		a[nElementos] = value;
-		 // inserta elemento
-		nElementos++;
-		 // incrementa
+	        /*
+		* Metodos 
+		*/
+		public void insert(long value){	 // metodo que agrega elementos dentro del  array (a)
+		a[nElementos] = value;	 // inserta elemento
+		nElementos++; 	 // incrementa
 		}
 		
-		public void mostrar(){
-		 // mostrar contenido del array 
-		
-		for(int j=0; j<nElementos; j++){   	 // for each  ,
-			System.out.print(a[j] + " ");        
-			System.out.println(" ");
-			}
+		public void mostrar(){	 // mostrar contenido del array 
+			for(int j=0; j<nElementos; j++){   	
+		            System.out.print(a[j] + " ");        
+			    System.out.println(" ");
+			} // end for(j)
 		}
-		
-		public void burbuja(){
+		public void burbuja(){ 
 		int salida, entrada;
-		for(salida=nElementos-1; salida>1; salida--) // bucle exterior (Hacia atras)
-		   for(entrada=0; entrada<salida; entrada++) // bucle interior (Hacia Adelante)
-			if( a[entrada] > a[entrada+1] ) // fuera de orden?
+		for(salida=nElementos-1; salida>1; salida--) //  (Hacia atras)
+		   for(entrada=0; entrada<salida; entrada++) //  (Hacia Adelante)
+			if( a[entrada] > a[entrada+1] ) 
 		intercambio(entrada, entrada+1);
+		 //no se cierra con llaves los cliclos for
+		//solo tiene una instruccion	
 		 // Intercambio
 		} // Fin Metodo burbuja()
 	
